045fa2ae1abfe2cbdc348c6faf8c2ab17fc887bc
Faith <kashfaith77@gmail.com>
2025-03-19 22:31:24 +0300

    liker app

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = "♡";
+const FULL_HEART = "♥";
 
 // Your JavaScript code goes here!
-
-
-
+let allLikes=document.querySelectorAll(".like-glyph")
+  allLikes.forEach((like)=>{
+  like.addEventListener("click", () => {
+    mimicServerCall()
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch(() => {
+        const modalHeading = document.querySelector("#modal h2"); // Select the h2 element
+        modalHeading.classList.remove("hidden");
+      });
+  });
+  
+})
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
-      let isRandomFailure = Math.random() < .2
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
